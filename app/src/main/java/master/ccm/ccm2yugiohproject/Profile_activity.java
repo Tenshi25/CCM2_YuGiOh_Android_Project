0f0807872a89d4321f32039ab6de00d80e2982f0
remig <urbaniec.remi@gmail.com>
2019-11-18 16:36:33 +0100

    Profile + correctif de bug

---
+++ app/src/main/java/master/ccm/ccm2yugiohproject/Profile_activity.java
@@ -0,0 +1,14 @@
+package master.ccm.ccm2yugiohproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}

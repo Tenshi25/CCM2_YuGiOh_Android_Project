7754396d47261e6c8ed27f91e499be872f10edfd
remig <urbaniec.remi@gmail.com>
2020-04-20 14:43:22 +0200

    IbotinvocationPropre

--- app/src/main/java/master/ccm/entity/IABot.java
+++ app/src/main/java/master/ccm/entity/IABot.java
@@ -1,11 +1,13 @@
 package master.ccm.entity;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 import master.ccm.entity.Effects.EffectMoveCardFromAtoB;
+import master.ccm.entity.PileDeCarte.Main;
 import master.ccm.entity.subcard.CardInGame;
 
 public class IABot {
@@ -13,11 +15,10 @@ public class IABot {
     private EffectMoveCardFromAtoB effectMoveCardFromAtoB = new EffectMoveCardFromAtoB();
     private Context context;
     private ArrayList<Player> listPlayer;
-    public void init (Context p_context, ArrayList<Player> p_listPlayer, Player p_player){
+    public void init (Context p_context, ArrayList<Player> p_listPlayer, Player p_player ){
         Iabot = p_player;
         context= p_context;
         listPlayer = p_listPlayer;
-
     }
 
     public void DeffauseIA(){
@@ -28,5 +29,46 @@ public class IABot {
         ArrayList<CardInGame> filtre = new ArrayList<>();
         filtre.add(aCard);
         effectMoveCardFromAtoB.execute(context,listPlayer,aCard.getPlayer().getNumJoueur(),1,Iabot.getPlayerMain(),Iabot.getPlayerCimetiere(),filtre);
+    }
+
+    public void invocationIA(ArrayList<CardInGame> listMonstre, Phase currentPhase) {
+        Random rand = new Random();
+        int action = rand.nextInt(2);
+        if (listMonstre.size() > 0) {
+            int nbinvocation = rand.nextInt(listMonstre.size());
+            CardInGame cardSelectRand = listMonstre.get(nbinvocation);
+            Main currentplayerMain = Iabot.getPlayerMain();
+            Log.w("action", "action : " + action);
+            switch (action) {
+                case 0:
+                    //invocation atk
+                    if (currentPhase.containsActionByName("InvocationNormale") && Iabot.equals(listPlayer.get(1))) {
+                        if (Iabot.getCountInvocationNormale() < Iabot.getMaxInvocationNormale()) {
+                            Action invocation = currentPhase.findActionByName("InvocationNormale");
+                            ArrayList<CardInGame> listfiltre = new ArrayList<>();
+                            listfiltre.add(cardSelectRand);
+                            invocation.getEffect().execute(context, listPlayer, 1, 1, Iabot.getPlayerMain(), Iabot.getPlayerTerrain(), listfiltre);
+                            //majbtAction(currentplayerMain.getSelectedCard());
+                        }
+                    }
+                case 1:
+                    //invocation def
+                    if (currentPhase.containsActionByName("PoseNormale") && Iabot.equals(listPlayer.get(1))) {
+                        if (Iabot.getCountInvocationNormale() < Iabot.getMaxInvocationNormale()) {
+                            Action invocation = currentPhase.findActionByName("PoseNormale");
+
+                            if (currentplayerMain.getSelectedCard() != null) {
+                                ArrayList<CardInGame> listfiltre = new ArrayList<>();
+                                listfiltre.add(cardSelectRand);
+                                //Log.w("ia invoq poser", "ia invoq poser : "+action);
+                                invocation.getEffect().execute(context, listPlayer, 1, 1, Iabot.getPlayerMain(), Iabot.getPlayerTerrain(), listfiltre);
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
